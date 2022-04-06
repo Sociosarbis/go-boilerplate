a595307caa6f8e9ffc8ee37f1653d0df91cf9f1b
sociosarbis <136657577@qq.com>
2022-04-06 20:37:34 +0800

    feat: :sparkles: (Depth-First Search Breadth-First Search Graph Topological Sort) 310. Minimum Height Trees

--- src/find/min/height/trees/trees.go
+++ src/find/min/height/trees/trees.go
@@ -1,20 +1,6 @@
 package trees
 
-func dfs(i int, neighbors *[][]int, visited *[]bool, temp int) int {
-	ret := temp + 1
-	(*visited)[i] = true
-	for _, neighbor := range (*neighbors)[i] {
-		if !(*visited)[neighbor] {
-			tempRet := dfs(neighbor, neighbors, visited, temp+1)
-			if tempRet > ret {
-				ret = tempRet
-			}
-		}
-	}
-	(*visited)[i] = false
-	return ret
-}
-
+// 等同于求树的中心节点
 func findMinHeightTrees(n int, edges [][]int) []int {
 	if n == 1 {
 		return []int{0}
@@ -24,17 +10,35 @@ func findMinHeightTrees(n int, edges [][]int) []int {
 		neighbors[edge[0]] = append(neighbors[edge[0]], edge[1])
 		neighbors[edge[1]] = append(neighbors[edge[1]], edge[0])
 	}
-	visited := make([]bool, n)
-	min := n
-	ret := []int{}
-	for i := range neighbors {
-		temp := dfs(i, &neighbors, &visited, 0)
-		if temp < min {
-			min = temp
-			ret = []int{i}
-		} else if temp == min {
-			ret = append(ret, i)
+
+	degrees := make([]int, n)
+
+	queue := []int{}
+	count := 0
+	for i, neighbor := range neighbors {
+		degrees[i] = len(neighbor)
+		if degrees[i] == 1 {
+			queue = append(queue, i)
+			degrees[i] = 0
 		}
 	}
-	return ret
+	count += len(queue)
+
+	for count < n {
+		nextQueue := []int{}
+		for _, i := range queue {
+			for _, neighbor := range neighbors[i] {
+				if degrees[neighbor] > 0 {
+					degrees[neighbor]--
+					if degrees[neighbor] == 1 {
+						nextQueue = append(nextQueue, neighbor)
+					}
+				}
+			}
+		}
+		queue = nextQueue
+		count += len(queue)
+	}
+
+	return queue
 }
