6893f41b7d8527db11a65b95d142a894110800de
sociosarbis <136657577@qq.com>
2023-02-06 12:55:34 +0800

    feat: :sparkles: (Tree Depth-First Search Binary Tree) 2331. Evaluate Boolean Binary Tree

---
+++ src/evaluate/tree/tree.go
@@ -0,0 +1,19 @@
+package tree
+
+type TreeNode struct {
+	Val   int
+	Left  *TreeNode
+	Right *TreeNode
+}
+
+func evaluateTree(root *TreeNode) bool {
+	if root.Left == nil {
+		return root.Val == 1
+	}
+
+	if root.Val == 2 {
+		return evaluateTree(root.Left) || evaluateTree(root.Right)
+	} else {
+		return evaluateTree(root.Left) && evaluateTree(root.Right)
+	}
+}
