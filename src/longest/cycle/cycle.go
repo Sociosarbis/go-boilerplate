ddfa9735effaf369a3de639104e75532be745434
sociosarbis <136657577@qq.com>
2025-03-29 12:48:59 +0800

    feat: :sparkles: (Depth-First Search Breadth-First Search Graph Topological Sort) 2360. Longest Cycle in a Graph

---
+++ src/longest/cycle/cycle.go
@@ -0,0 +1,63 @@
+package cycle
+
+import "fmt"
+
+func longestCycle(edges []int) int {
+	n := len(edges)
+	inDeg := make([]int, n)
+	for _, edge := range edges {
+		if edge != -1 {
+			inDeg[edge]++
+		}
+	}
+	visited := make([]bool, n)
+	queue := make([]int, 0, n)
+	for i, deg := range inDeg {
+		if deg == 0 {
+			visited[i] = true
+			queue = append(queue, i)
+		}
+	}
+	l := len(queue)
+	var ret int
+	for l != 0 {
+		for i := 0; i < l; i++ {
+			next := edges[queue[i]]
+			if next != -1 {
+				if inDeg[next] > 0 {
+					inDeg[next]--
+					if inDeg[next] == 0 {
+						visited[next] = true
+						queue = append(queue, next)
+					}
+				}
+			}
+		}
+		queue = queue[l:]
+		l = len(queue)
+	}
+	fmt.Println(visited)
+	for i := 0; i < n; i++ {
+		if !visited[i] {
+			var temp int
+			next := edges[i]
+			fmt.Println(next)
+			for next != -1 {
+				temp++
+				fmt.Println(temp)
+				if next == i {
+					if temp > ret {
+						ret = temp
+					}
+					break
+				}
+				visited[next] = true
+				next = edges[next]
+			}
+		}
+	}
+	if ret == 0 {
+		return -1
+	}
+	return ret
+}
