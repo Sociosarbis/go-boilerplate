7c8199641ea4e134d245738dd479def96e80cf75
sociosarbis <136657577@qq.com>
2026-05-17 12:09:34 +0800

    feat: :sparkles: (Depth-First Search Breadth-First Search) 1306. Jump Game III

---
+++ src/can/jump/jump.go
@@ -0,0 +1,32 @@
+package jump
+
+func canReach(arr []int, start int) bool {
+	if arr[start] == 0 {
+		return true
+	}
+	n := len(arr)
+	visited := make([]bool, n)
+	visited[start] = true
+	multipliers := []int{-1, 1}
+	bfs := make([]int, 0, n)
+	bfs = append(bfs, start)
+	l := 1
+	for l != 0 {
+		for i := 0; i < l; i++ {
+			index := bfs[i]
+			for _, multiplier := range multipliers {
+				next := index + arr[index]*multiplier
+				if next >= 0 && next < n && !visited[next] {
+					if arr[next] == 0 {
+						return true
+					}
+					visited[next] = true
+					bfs = append(bfs, next)
+				}
+			}
+		}
+		bfs = bfs[l:]
+		l = len(bfs)
+	}
+	return false
+}
